5266290c3f5ec9aa64c38a732b41d832e090234b
Veli ÖRNEK <chankouteh@hotmail.com>
2019-09-08 21:51:49 +0300

    initial submit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace anti_ransomware
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnFixAtr = new System.Windows.Forms.Button();
+            this.btnFixPath = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnFixAtr
+            // 
+            this.btnFixAtr.Location = new System.Drawing.Point(12, 12);
+            this.btnFixAtr.Name = "btnFixAtr";
+            this.btnFixAtr.Size = new System.Drawing.Size(100, 23);
+            this.btnFixAtr.TabIndex = 0;
+            this.btnFixAtr.Text = "Fix Attributes";
+            this.btnFixAtr.UseVisualStyleBackColor = true;
+            this.btnFixAtr.Click += new System.EventHandler(this.btnFixAtr_Click);
+            // 
+            // btnFixPath
+            // 
+            this.btnFixPath.Location = new System.Drawing.Point(195, 12);
+            this.btnFixPath.Name = "btnFixPath";
+            this.btnFixPath.Size = new System.Drawing.Size(100, 23);
+            this.btnFixPath.TabIndex = 1;
+            this.btnFixPath.Text = "Fix Path Issues";
+            this.btnFixPath.UseVisualStyleBackColor = true;
+            this.btnFixPath.Click += new System.EventHandler(this.btnFixPath_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(307, 43);
+            this.Controls.Add(this.btnFixPath);
+            this.Controls.Add(this.btnFixAtr);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmMain";
+            this.Text = "Fix Ransomware Issue";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFixAtr;
+        private System.Windows.Forms.Button btnFixPath;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace anti_ransomware
+{
+    public partial class frmMain : Form
+    {
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnFixAtr_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = Directory.GetCurrentDirectory();
+                string[] allfiles = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+                string[] directories = Directory.GetDirectories(path,"*",SearchOption.AllDirectories);
+ 
+                if (allfiles != null)
+                {
+                    if (allfiles.Length != 0)
+                    {
+                        foreach (string currentFile in allfiles)
+                        {
+                            try
+                            {
+                                File.SetAttributes(currentFile, FileAttributes.Normal);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("The process failed: {0} for {1}", ex.StackTrace, currentFile);
+                            }
+                        }
+
+                        foreach (string currentFile in directories)
+                        {
+                            try
+                            {
+                                File.SetAttributes(currentFile, FileAttributes.Normal);
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("The process failed: {0} for {1}", ex.StackTrace, currentFile);
+                            }
+                        }
+
+                        MessageBox.Show("Done!", "Operation Successful");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Nothing To Fix");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Nothing To Fix");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.StackTrace, "Error");
+                Console.WriteLine("The process failed: {0}", ex.StackTrace);
+            }
+        }
+
+        private void btnFixPath_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string path = Directory.GetCurrentDirectory();
+                string target = path + "\\fixed";
+                string[] allfiles = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+
+                if (allfiles != null)
+                {
+                    if (allfiles.Length != 0)
+                    {
+                        foreach (string currentFile in allfiles)
+                        {
+                            try
+                            {
+                                string fName = Path.GetFileNameWithoutExtension(currentFile);
+                                string ext = Path.GetExtension(currentFile);
+                                string dest = Path.Combine(target, fName + ext);
+                                if (Path.GetFileName(currentFile).Equals(System.AppDomain.CurrentDomain.FriendlyName) 
+                                    || Path.GetDirectoryName(currentFile).Equals(target) 
+                                    || Path.GetDirectoryName(currentFile).Equals(path))
+                                {
+                                    continue;
+                                }
+
+                                if (File.Exists(dest))
+                                {
+                                    File.Move(currentFile, MakeUnique(dest));
+                                }
+                                else
+                                {
+                                    string dir = Path.GetDirectoryName(dest);
+                                    if (!Directory.Exists(dir))
+                                    {
+                                        Directory.CreateDirectory(dir);
+                                    }
+                                    
+                                    File.Move(currentFile, dest);
+                                }
+
+                            }
+                            catch (Exception ex)
+                            {
+                                Console.WriteLine("The process failed: {0} for {1}", ex.StackTrace, currentFile);
+                            }
+                        }
+                        CleanUpEmptyDirectories(path);
+                        MessageBox.Show("Done!", "Operation Successful");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Nothing To Fix", "Operation Successful");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Nothing To Fix","Operation Successful");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.StackTrace,"Error");
+                Console.WriteLine("The process failed: {0}", ex.StackTrace);
+            }
+        }
+
+        public string MakeUnique(string path)
+        {
+            string extension = Path.GetExtension(path);
+            string pathName = Path.GetDirectoryName(path);
+            string fileNameOnly = Path.Combine(pathName, Path.GetFileNameWithoutExtension(path));
+            int i = 1;
+
+            while (File.Exists(path))
+            {
+                path = string.Format("{0} ({1}){2}", fileNameOnly, i++, extension);
+            }
+            return path;
+        }
+
+        public void CleanUpEmptyDirectories(string path)
+        {
+            string[] dirs = Directory.GetDirectories(path, "*", SearchOption.AllDirectories);
+            foreach (string dir in dirs)
+            {
+                if (!Directory.Exists(dir)) continue;
+                string[] files = Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories);
+                if (files.Length == 0)
+                {
+                    DeleteRecursively(dir);
+                }
+            }
+        }
+
+        private void DeleteRecursively(string destinationDir)
+        {
+            const int md = 10;
+            for (var gnm = 1; gnm <= md; gnm++)
+            {
+                try
+                {
+                    Directory.Delete(destinationDir, true);
+                }
+                catch (DirectoryNotFoundException)
+                {
+                    return; 
+                }
+                catch (IOException)
+                { 
+                    System.Diagnostics.Debug.WriteLine("Deletion of {0} failed! attempt #{1}.", destinationDir, gnm);
+                    Thread.Sleep(50);
+                    continue;
+                }
+                return;
+            }
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+            this.FormBorderStyle = FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+        }
+    }
+}
